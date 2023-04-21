ec3fdcfc6af67b9051fea469be2f36e12d6f3b86
SergY29 <yurtin.sergey.v@gmail.com>
2023-04-21 16:56:48 +0300

    Create initAuth.js

---
+++ src/redux/auth/initAuth.js
@@ -0,0 +1,8 @@
+export const initAuth = {
+    user: { name: null, email: null, password: null },
+    token: null,
+    isLoggedIn: true,
+    isRefreshing: false,
+    isTheme: true,
+    error: false,
+}
