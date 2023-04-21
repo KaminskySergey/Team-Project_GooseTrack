37ac36578a09ffaf1be794f2b64575bb3f3bfbd5
SergY29 <yurtin.sergey.v@gmail.com>
2023-04-21 14:14:38 +0300

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import { useEffect, lazy } from 'react';
 import { Layout } from './Layout';
+import { RestrictedRoute } from 'components/RestrictedRoute';
+import { PrivateRoute } from 'components/PrivateRoute';
 
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
+const CalendarPage = lazy(() => import('pages/CalendarPage/CalendarPage'));
 
 export const App = () => {
   return (
@@ -11,6 +14,15 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute
+              redirectTo="/calendar"
+              component={<CalendarPage />}
+            />
+          }
+        />
       </Route>
     </Routes>
   );
