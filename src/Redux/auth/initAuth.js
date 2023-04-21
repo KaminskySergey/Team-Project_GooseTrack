29a28fa897f5c3c1e38a900b551f32c1dbfc3d3a
SergY29 <106399401+SergY29@users.noreply.github.com>
2023-04-21 16:52:25 +0300

    Delete src/Redux directory

--- src/Redux/auth/initAuth.js
+++
@@ -1,8 +0,0 @@
-export const initAuth = {
-    user: { name: null, email: null, password: null },
-    token: null,
-    isLoggedIn: true,
-    isRefreshing: false,
-    isTheme: true,
-    error: false,
-}
