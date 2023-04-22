1c919a033a735740bf23a0d322d7a3316efc9686
Inna <isko.inna.94@gmail.com>
2023-04-22 18:40:53 +0300

    todolist

--- src/images/svg/index.js
+++ src/images/svg/index.js
@@ -1,5 +1,7 @@
 import iconDelete from './iconDelete.svg';
 import iconEdit from './iconEdit.svg';
 import iconMove from './iconMove.svg';
+import plusAdd from './plusAdd.svg';
+import addIcon from './addIcon.svg';
 
-export { iconDelete, iconEdit, iconMove };
+export { iconDelete, iconEdit, iconMove, plusAdd, addIcon };
