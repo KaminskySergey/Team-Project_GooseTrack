025dc2f0a2fc8efd3617e21539385d074b19f608
Valerii Droniak <droniak.valerii@gmail.com>
2023-04-24 21:03:56 +0300

    editing style

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -7,13 +7,13 @@ export const Wrapper = styled.div`
 
 export const Container = styled.div`
   outline: 1px solid tomato;
-  /* height: 100vh; */
+  min-height: 100vh;
   width: 100%;
 `;
 
 export const Main = styled.div`
   width: 100%;
-  height: 100%;
+  min-height: 100vh;
   background-color: #dce3e5;
   padding: 40px 32px 32px 32px;
 `;
