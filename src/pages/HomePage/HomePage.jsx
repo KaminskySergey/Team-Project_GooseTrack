f047f0dad99773ac57b70895034d70b1cb5626a2
Inna <isko.inna.94@gmail.com>
2023-04-24 14:43:12 +0300

    3

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,6 @@
 import {} from 'react-router-dom';
 
 import {} from './HomePage.styled';
-import { ChoosedDay } from '../../components/ChoosedDay/ChoosedDay';
 import { AuthNavigate } from 'components/AuthNavigate';
 
 export default function HomePage() {
@@ -16,7 +15,6 @@ export default function HomePage() {
           💁‍♀️
         </span>
       </h1>
-      <ChoosedDay />
       <nav>
         <AuthNavigate redirect={redirectLogin} nameLink="Log in" />
         <AuthNavigate redirect={redirectRegister} nameLink="Sing up" />
