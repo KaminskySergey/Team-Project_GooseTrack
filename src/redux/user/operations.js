6ce50e3f618a961dd2ce71811e01a4327d46e8c9
Valerii Droniak <droniak.valerii@gmail.com>
2023-04-25 20:53:30 +0300

    fixed resalt

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -5,8 +5,8 @@ export const fetchUser = createAsyncThunk(
   'user/fetchUser',
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get('/user/current');
-      return data.result.user;
+      const { data: { data } } = await axios.get('/user/current');
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -17,8 +17,8 @@ export const updateUser = createAsyncThunk(
   'user/updateUser',
   async (credentials, thunkAPI) => {
     try {
-      const { data } = await axios.patch('/user/info', credentials);
-      return data.result.user;
+      const { data: { data } } = await axios.patch('/user/info', credentials);
+      return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
