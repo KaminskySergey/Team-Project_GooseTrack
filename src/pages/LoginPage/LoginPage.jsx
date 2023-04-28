324eb7ed4040dc6f3b42d4f821a61a9d94983323
SergY29 <106399401+SergY29@users.noreply.github.com>
2023-04-28 15:29:42 +0300

    add main style login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,70 +1,15 @@
-import {} from './LoginPage.styled';
+import { Сontainer } from './LoginPage.styled';
 
 import { LoginForm } from 'components/LoginForm';
 import { AuthNavigate } from 'components/AuthNavigate';
-import { login } from '../../redux/auth/authOperations';
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
-
 
 export default function LoginPage() {
   const redirectRegister = '/register';
-  const dispatch = useDispatch()
-  
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
 
-  const handleChange = (e) => {
-    
-    
-    const {name, value} = e.target
-    switch (name) {
-      
-        case 'email':
-          setEmail(value)
-        break;
-        case 'password':
-          setPassword(value)
-        break;
-    
-      default:
-        break;
-    }
-  }
-  
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    dispatch(login({email, password}))
-    console.log({email, password})
-
-    
-    setEmail('')
-    setPassword('')
-  }
   return (
-    <div>
-      <h1>
-        Login Page
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
-      <form onSubmit={handleSubmit} style={{outline: '1px solid black'}}>
-        
-        <label>
-          Email
-          <input type="email" value={email} name='email' onChange={handleChange}/>
-        </label>
-        <label>
-          Password
-          <input type="password" value={password} name='password' onChange={handleChange}/>
-        </label>
-        <button type='submit'>submit</button>
-      </form>
-
+    <Сontainer>
       <LoginForm />
-      <AuthNavigate redirect={redirectRegister} nameLink="Log in" />
-    </div>
+      <AuthNavigate redirect={redirectRegister} nameLink="Sign up" />
+    </Сontainer>
   );
 }
