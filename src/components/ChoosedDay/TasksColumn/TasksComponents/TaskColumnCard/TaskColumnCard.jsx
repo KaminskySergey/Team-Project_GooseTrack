ad9628da8fea91b3e4c71a9fbdc7809ce0b3d9ea
KaminskySergey <kaminsky_s_s@ukr.net>
2023-04-29 17:19:33 +0300

    fix

--- src/components/ChoosedDay/TasksColumn/TasksComponents/TaskColumnCard/TaskColumnCard.jsx
+++ src/components/ChoosedDay/TasksColumn/TasksComponents/TaskColumnCard/TaskColumnCard.jsx
@@ -11,11 +11,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectIsitems } from 'redux/tasks/selectors';
 import { deleteTasks } from 'redux/tasks/operations';
 
-const data = [
-  {name: 'gbgbgbgbg', avatar: 'https://klike.net/uploads/posts/2019-07/1564314059_1.jpg', status: 'high'},
-  {name: 'gbgbgbgb', avatar: 'https://klike.net/uploads/posts/2019-07/1564314059_1.jpg', status: 'medium'},
-  {name: 'qwqweqweqwewqe', avatar: 'https://klike.net/uploads/posts/2019-07/1564314059_1.jpg', status: 'surer'},
-]
+
 
 
 export const TaskColumnCard = () => {
@@ -25,7 +21,7 @@ export const TaskColumnCard = () => {
   const handleDeleteTask = (taskId) => {
     dispatch(deleteTasks(taskId))
   }
-console.log(items, 'avatar')
+
   return (
     <>
     {
