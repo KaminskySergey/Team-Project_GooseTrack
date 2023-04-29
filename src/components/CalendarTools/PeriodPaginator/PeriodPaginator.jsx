ad4c78f13daefdb39369aa7bb53fc0ffaa66d5d1
KaminskySergey <kaminsky_s_s@ukr.net>
2023-04-29 17:22:20 +0300

    опять

--- src/components/CalendarTools/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/CalendarTools/PeriodPaginator/PeriodPaginator.jsx
@@ -1,12 +1,10 @@
 import { format } from "date-fns";
 import { CurrentDate, CurrentWrapper, BiChevronLeftStyled, BiChevronRightStyled, ButtonPeriodPaginator, ConteinerForButtons } from "./PeriodPaginator.styled";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
-import { fetchTasksAll } from "redux/tasks/operations";
+
 
 export const PeriodPaginator = ({isChoosedMonth, changeAskDay, calendar, askDay}) => {
     const date = isChoosedMonth ? format(askDay, 'MMMM yyyy') : format(askDay, 'dd MMMM yyyy');
-    console.log(date, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
+    
     
     
   
