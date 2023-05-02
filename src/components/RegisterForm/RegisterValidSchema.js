d5f1e76c204b0a9077495d68e684402ed3cac839
SergY29 <106399401+SergY29@users.noreply.github.com>
2023-05-03 02:18:38 +0300

    fixed validation length name

--- src/components/RegisterForm/RegisterValidSchema.js
+++ src/components/RegisterForm/RegisterValidSchema.js
@@ -7,7 +7,7 @@ export const RegisterValidSchema = Yup.object().shape({
     name: Yup.string()
         .required('Name is required')
         .min(3, 'Name is too short')
-        .max(64, 'Name is too long'),
+        .max(16, 'Name is too long'),
     email: Yup.string().email('Invalid email').required('Required'),
     password: Yup.string()
         .min(5, 'Password is too short')
