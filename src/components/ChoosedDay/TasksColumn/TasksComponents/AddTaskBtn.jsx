fcd358b8d7c53359f96d34053b05405fbc89f45d
SergY29 <106399401+SergY29@users.noreply.github.com>
2023-05-02 10:26:08 +0300

    add task btn

--- src/components/ChoosedDay/TasksColumn/TasksComponents/AddTaskBtn.jsx
+++ src/components/ChoosedDay/TasksColumn/TasksComponents/AddTaskBtn.jsx
@@ -1,49 +1,44 @@
-import ModalCreate from "components/ModalCreate/ModalCreate";
-import ModalTodo from "pages/ModalTodo/ModalTodo";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addTasks } from "redux/tasks/operations";
+import ModalCreate from 'components/ModalCreate/ModalCreate';
+import ModalTodo from 'pages/ModalTodo/ModalTodo';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTasks } from 'redux/tasks/operations';
 
+export const AddTaskBtn = ({ listId }) => {
+  const [isModalOpen, setIsOpenModal] = useState(false);
 
-export const AddTaskBtn = () => {
-  const [isModalOpen, setIsOpenModal] = useState(false)
-  
   // const [isAddTodo, setIsAddTodo] = useState(false)
   // const [isPatchTodo, setIsPatchTodo] = useState(false)
 
+  const dispatch = useDispatch();
 
-  
-
-  const dispatch = useDispatch()
-
-  
-  
-  const handleAddTodo = (value) => {
+  const handleAddTodo = value => {
     const now = new Date();
-  const date = new Date(now)
-  const isoString = date.toISOString();
-// Das ist ToDo!!!
-  const todo = {
-    date: isoString,
-    ...value,
-    category: 'toDo'
+    const date = new Date(now);
+    const isoString = date.toISOString();
+    // Das ist ToDo!!!
+    const todo = {
+      date: isoString,
+      ...value,
+      category: listId,
+    };
 
-  }
-  console.log(todo, 'todo')
-    dispatch(addTasks(todo))
-    handleToggle()
-  }
+    dispatch(addTasks(todo));
+    handleToggle();
+  };
 
-  
-  const handleToggle = () => setIsOpenModal(pS => !pS)
-  
-   return (
-   <>
-   <button type="button" onClick={handleToggle}>ADD TODO</button>
-    {isModalOpen && 
-    <ModalCreate onClose={handleToggle}>
-      <ModalTodo handleAddTodo={handleAddTodo}/>
-    </ModalCreate>
-    }
-   </>)
+  const handleToggle = () => setIsOpenModal(pS => !pS);
+
+  return (
+    <>
+      <button type="button" onClick={handleToggle}>
+        ADD TODO
+      </button>
+      {isModalOpen && (
+        <ModalCreate onClose={handleToggle}>
+          <ModalTodo handleAddTodo={handleAddTodo} />
+        </ModalCreate>
+      )}
+    </>
+  );
 };
