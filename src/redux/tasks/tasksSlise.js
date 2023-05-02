4504a129a4bacbb43a74e3e92d3ec5b0bb6082b6
SergY29 <106399401+SergY29@users.noreply.github.com>
2023-05-02 21:33:35 +0300

    Update tasksSlise.js

--- src/redux/tasks/tasksSlise.js
+++ src/redux/tasks/tasksSlise.js
@@ -1,3 +1,4 @@
+
 import { createSlice } from '@reduxjs/toolkit';
 import { initTasks } from './initTasks';
 
@@ -16,44 +17,6 @@ export const tasksSlise = createSlice({
   initialState: initTasks,
   extraReducers: builder =>
     builder
-      //=========transferTask
-      .addCase(transferTask.pending, state => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(transferTask.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        const { _id, category } = action.payload.task;
-
-        const done = state.done.find(task => task._id === _id);
-        const todo = state.todo.find(task => task._id === _id);
-        const inProgress = state.inProgress.find(task => task._id === _id);
-
-
-        if (!todo && !done && !inProgress) return;
-        // Удаляем задачу из предыдущей категории
-        if (done) {
-          state[done.category] = state[done.category].filter(t => t._id !== _id);
-        }
-        if (todo) {
-
-          state[todo.category] = state[todo.category].filter(t => t._id !== _id);
-        }
-        if (inProgress) {
-          state[inProgress.category] = state[inProgress.category].filter(t => t._id !== _id);
-        }
-
-        // Добавляем задачу в новую категорию
-        state[category].push(action.payload.task);
-        state[category] = [...state[category],]
-      })
-      .addCase(transferTask.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload ? action.payload.message : action.error.message;
-      })
-
-
       //========fetchAllTasks
       .addCase(fetchTasksAll.pending, (state, action) => {
         // handlePending();
@@ -134,6 +97,42 @@ export const tasksSlise = createSlice({
         state.done = state.done.filter(task => task._id !== action.payload.task._id)
       })
       //========editTasks
+      .addCase(transferTask.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(transferTask.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const { _id, category } = action.payload.task;
+
+        const done = state.done.find(task => task._id === _id);
+        const todo = state.todo.find(task => task._id === _id);
+        const inProgress = state.inProgress.find(task => task._id === _id);
+
+
+        if (!todo && !done && !inProgress) return;
+        // Удаляем задачу из предыдущей категории
+        if (done) {
+          state[done.category] = state[done.category].filter(t => t._id !== _id);
+        }
+        if (todo) {
+
+          state[todo.category] = state[todo.category].filter(t => t._id !== _id);
+        }
+        if (inProgress) {
+          state[inProgress.category] = state[inProgress.category].filter(t => t._id !== _id);
+        }
+
+        // Добавляем задачу в новую категорию
+        state[category].push(action.payload.task);
+        state[category] = [...state[category],]
+      })
+      .addCase(transferTask.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload ? action.payload.message : action.error.message;
+      })
+
       .addCase(editTasks.pending, (state, action) => {
 
         state.isLoading = true;
@@ -150,6 +149,7 @@ export const tasksSlise = createSlice({
 
         state.todo = state.todo.map(task => {
           if (task._id === action.payload.task._id) {
+
             return action.payload.task;
           } else {
             return task;
@@ -171,5 +171,11 @@ export const tasksSlise = createSlice({
             return task;
           }
         });
-      }),
+      }
+      )
+
+
+
+
+
 });
