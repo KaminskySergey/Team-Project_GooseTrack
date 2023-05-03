4da81ece45daea5658a3e8380079f987c2516bc3
Valerii Droniak <droniak.valerii@gmail.com>
2023-05-03 10:21:28 +0300

    fixed error message

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -46,8 +46,8 @@ export const UserForm = () => {
           await dispatch(updateUser(values));
 
           toast.success('Profile is successfully updated');
-        } catch {
-          toast.error('Login or password wrong');
+        } catch (errror) {
+          toast.error(errror.message);
         }
       }}
     >
