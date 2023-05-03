a02aaaba1949275b3a693c08966b5f0e09bb0fa2
OlexandrKostenko <mensionss@gmail.com>
2023-05-03 21:11:10 +0300

    LinksPeriodPaginator fixed

--- src/components/CalendarTools/PeriodPaginator/PeriodPaginator.jsx
+++ src/components/CalendarTools/PeriodPaginator/PeriodPaginator.jsx
@@ -1,10 +1,13 @@
-import { format } from "date-fns";
-import { CurrentDate, CurrentWrapper, BiChevronLeftStyled, BiChevronRightStyled, ButtonPeriodPaginator, ConteinerForButtons } from "./PeriodPaginator.styled";
+import { format, subDays, addDays, subMonths, addMonths } from "date-fns";
+import { CurrentDate, CurrentWrapper, BiChevronLeftStyled, BiChevronRightStyled, LinkPeriodPaginator, ConteinerForButtons } from "./PeriodPaginator.styled";
 
 
-export const PeriodPaginator = ({isChoosedMonth, changeAskDay, calendar, askDay}) => {
+export const PeriodPaginator = ({isChoosedMonth, changeAskDay, askDay}) => {
     const date = isChoosedMonth ? format(askDay, 'MMMM yyyy') : format(askDay, 'dd MMMM yyyy');
-    
+    const nextDay = format(addDays(askDay, 1), 'ddMMMMyyyy');
+    const prevDay = format(subDays(askDay, 1), 'ddMMMMyyyy');
+    const nextMonth = format(addMonths(askDay, 1), 'MMMMyyyy');
+    const prevMonth = format(subMonths(askDay, 1), 'MMMMyyyy');
     
     
   
@@ -12,8 +15,14 @@ export const PeriodPaginator = ({isChoosedMonth, changeAskDay, calendar, askDay}
         <CurrentWrapper>
             <CurrentDate>{date}</CurrentDate>
             <ConteinerForButtons>
-                <ButtonPeriodPaginator onClick={() => changeAskDay(-1)}><BiChevronLeftStyled/></ButtonPeriodPaginator>
-                <ButtonPeriodPaginator onClick={() => changeAskDay(1)}><BiChevronRightStyled/></ButtonPeriodPaginator>
+                {!isChoosedMonth ? 
+                (<><LinkPeriodPaginator to={`/calendar/day/${prevDay}`} onClick={() => changeAskDay(-1)}><BiChevronLeftStyled/></LinkPeriodPaginator>
+                <LinkPeriodPaginator to={`/calendar/day/${nextDay}`} onClick={() => changeAskDay(1)}><BiChevronRightStyled/></LinkPeriodPaginator></>) 
+                :
+                (<><LinkPeriodPaginator to={`/calendar/month/${prevMonth}`} onClick={() => changeAskDay(-1)}><BiChevronLeftStyled/></LinkPeriodPaginator>
+                <LinkPeriodPaginator to={`/calendar/month/${nextMonth}`} onClick={() => changeAskDay(1)}><BiChevronRightStyled/></LinkPeriodPaginator></>)
+                }
+                
             </ConteinerForButtons>
         </CurrentWrapper>
     )
--- src/components/CalendarTools/PeriodPaginator/PeriodPaginator.styled.jsx
+++ src/components/CalendarTools/PeriodPaginator/PeriodPaginator.styled.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
+import { NavLink } from "react-router-dom";
 
 export const BiChevronLeftStyled = styled(BiChevronLeft)`
-height: 30px;
-width: 36px;
+
 fill: #616161;
 `;
 
@@ -20,7 +20,7 @@ padding: 6px 12px;
 background: ${props => props.theme.accentColor};
 border-radius: 8px;
 color: ${props => props.theme.secondaryTextColor};
-font-family: 'Inter';
+font-family: 'InterBolt';
 font-style: normal;
 font-weight: 700;
 font-size: 14px;
@@ -43,7 +43,7 @@ display: flex;
 justify-content: space-between;
 margin-bottom: 18px;`;
 
-export const ButtonPeriodPaginator = styled.button`
+export const LinkPeriodPaginator = styled(NavLink)`
 width: 36px;
 height: 34px;
 box-sizing: border-box;
